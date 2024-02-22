ae2d1e8c32f2509e707f052f9683ab7adffcc66d
Antony Gakuru <antony123muriithi@gmail.com>
2024-02-22 21:54:58 +0300

    add redux

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { createStackNavigator, TransitionPresets } from "@react-navigation/stack";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider } from 'react-redux';
+import store from './store';
 import Home from './screens/home';
 import Login from './screens/login';
 import SignUp from './screens/signUp';
@@ -13,66 +15,68 @@ import Settings from './screens/settingsPage';
 import Routine from './screens/plannerScreens/routine';
 
 const App = () => {
-    const Stack = createStackNavigator()
-    const horizontalAnimation = {
-        gestureDirection: 'horizontal',
-        cardStyleInterpolator: ({ current, layouts }) => {
-            return {
-                cardStyle: {
-                    transform: [
-                        {
-                            translateX: current.progress.interpolate({
-                                inputRange: [0, 1],
-                                outputRange: [layouts.screen.width, 0],
-                            }),
-                        },
-                    ],
-                },
-            };
-        },
-    };
-    const slideFromLeftAnimation = {
-        gestureDirection: 'horizontal-inverted',
-        cardStyleInterpolator: ({ current, layouts }) => {
-          return {
-            cardStyle: {
-              transform: [
-                {
-                  translateX: current.progress.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [-layouts.screen.width, 0],
-                  }),
-                },
-              ],
+  const Stack = createStackNavigator()
+  const horizontalAnimation = {
+    gestureDirection: 'horizontal',
+    cardStyleInterpolator: ({ current, layouts }) => {
+      return {
+        cardStyle: {
+          transform: [
+            {
+              translateX: current.progress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [layouts.screen.width, 0],
+              }),
             },
-          };
+          ],
         },
       };
-    return (
+    },
+  };
+  const slideFromLeftAnimation = {
+    gestureDirection: 'horizontal-inverted',
+    cardStyleInterpolator: ({ current, layouts }) => {
+      return {
+        cardStyle: {
+          transform: [
+            {
+              translateX: current.progress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [-layouts.screen.width, 0],
+              }),
+            },
+          ],
+        },
+      };
+    },
+  };
+  return (
+    <Provider store={store}>
       <PaperProvider>
         <GestureHandlerRootView style={{ flex: 1 }}>
-            <Stack.Navigator
-                screenOptions={{
-                    header: () => null,
-                    animationTypeForReplace: 'push',
-                    gestureEnabled: true,
-                    ...horizontalAnimation, 
-                }}
-            >
-                <Stack.Screen name="Login" component={Login} />
-                {/* <Stack.Screen name="Home" component={Home} /> */}
-                <Stack.Screen name="SideMenu" component={SideMenu} options={slideFromLeftAnimation}/>
-                <Stack.Screen name="SignUp" component={SignUp} />
-                <Stack.Screen name="Planner" component={Planner} />
-                <Stack.Screen name="Reminders" component={Reminders} />
-                <Stack.Screen name="Settings" component={Settings} />
-                <Stack.Screen name="Routine" component={Routine} />
+          <Stack.Navigator
+            screenOptions={{
+              header: () => null,
+              animationTypeForReplace: 'push',
+              gestureEnabled: true,
+              ...horizontalAnimation,
+            }}
+          >
+            <Stack.Screen name="Login" component={Login} />
+            {/* <Stack.Screen name="Home" component={Home} /> */}
+            <Stack.Screen name="SideMenu" component={SideMenu} options={slideFromLeftAnimation} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="Planner" component={Planner} />
+            <Stack.Screen name="Reminders" component={Reminders} />
+            <Stack.Screen name="Settings" component={Settings} />
+            <Stack.Screen name="Routine" component={Routine} />
 
 
-            </Stack.Navigator>
+          </Stack.Navigator>
         </GestureHandlerRootView>
       </PaperProvider>
-    )
+    </Provider>
+  )
 }
 
 export default App
---
+++ app/reducers/authReducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+    user: null,
+    isAuthenticated: false,
+    loading: false,
+    error: null
+};
+
+const authReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'LOGIN_SUCCESS':
+            return {
+                ...state,
+                user: action.payload,
+                isAuthenticated: true,
+                loading: false,
+                error: null
+            };
+        case 'LOGIN_FAILURE':
+            return {
+                ...state,
+                user: null,
+                isAuthenticated: false,
+                loading: false,
+                error: action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+export default authReducer;
