eaa4cc2f61bc5cc07e6d7514fe012c38bd09e07e
Antony Gakuru <antony123muriithi@gmail.com>
2024-02-23 21:09:18 +0300

    add promise method

--- app/screens/signUp.jsx
+++ app/screens/signUp.jsx
@@ -1,10 +1,93 @@
-import { View, Text, SafeAreaView, ImageBackground, TouchableOpacity, TextInput, KeyboardAvoidingView, ScrollView } from 'react-native';
+import { View, Text, SafeAreaView, ImageBackground, TouchableOpacity, TextInput, KeyboardAvoidingView, ScrollView, ActivityIndicator } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react'
+import React, { useState } from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Link } from 'expo-router';
+import { useToast } from "react-native-toast-notifications";
+import BASE_URL from '../baseUrl';
+
 
 const SignUp = () => {
+    const toast = useToast();
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({
+        first_name: "",
+        last_name: "",
+        username: "",
+        age: "",
+        email: "",
+        password: "",
+    });
+    const [isLoading, setIsLoading] = useState(false);
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+
+    const validateEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
+    const validatePassword = (password) => {
+        return password.length >= 6;
+    }
+    function handleShowPassword() {
+        setShowPassword(!showPassword);
+    }
+    async function handleSignUp() {
+        if (!validateEmail(formData.email)) {
+            setEmailError("Invalid email address");
+            return;
+        }
+        if (!validatePassword(formData.password)) {
+            setPasswordError("Password must be at least 6 characters");
+            return;
+        }
+        setIsLoading(true)
+        try {
+            const response = await fetch(`${BASE_URL}/api/v1/users`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(formData)
+            })
+            if (response.ok) {
+                console.log(response.status)
+                const data = await response.json()
+                console.log(data, 'from login')
+                navigation.replace('Home');
+                dispatch(loginSuccess(data))
+            }
+            else {
+                console.log(response.status)
+                toast.show("Invalid email or password", {
+                    type: "danger",
+                    placement: "top",
+                    duration: 4000,
+                    offset: 30,
+                    animationType: "zoom-in",
+                    swipeEnabled: true
+                });
+
+            }
+
+        }
+        catch (error) {
+            toast.show("There was a problem with the fetch operation", {
+                type: "danger",
+                placement: "bottom",
+                duration: 4000,
+                offset: 30,
+                animationType: "zoom-in",
+            });
+            console.error('There was a problem with the fetch operation:', error.message);
+        }
+        finally {
+            setIsLoading(false);
+        };
+    }
     return (
         <View className="w-full h-full">
             <ImageBackground className=" w-full h-auto flex-1" source={require("../assets/images/loginBg.png")}>
@@ -35,43 +118,68 @@ const SignUp = () => {
 
                             <KeyboardAvoidingView className="">
                                 <TextInput
-                                    placeholder="username"
+                                    placeholder="first name"
                                     className="mb-3 mt-5 border border-gray-300 rounded-full px-6 py-2 text-md text-gray-500 "
-                                // value={inputText}
-                                // onChangeText={handleInputChange}
+                                    value={formData.first_name}
+                                    onChangeText={(first_name) => setFormData({ ...formData, first_name })}
+                                />
+                                <TextInput
+                                    placeholder="last name"
+                                    className="mb-3 border border-gray-300 rounded-full px-6 py-2 text-md text-gray-500 "
+                                    value={formData.last_name}
+                                    onChangeText={(last_name) => setFormData({ ...formData, last_name })}
+                                />
+                                <TextInput
+                                    placeholder="username"
+                                    className="mb-3 border border-gray-300 rounded-full px-6 py-2 text-md text-gray-500 "
+                                    value={formData.username}
+                                    onChangeText={(username) => setFormData({ ...formData, username })}
                                 />
                                 <TextInput
                                     placeholder="age"
                                     className="mb-3 border border-gray-300 rounded-full px-6 py-2 text-md text-gray-500"
-                                // value={inputText}
-                                // onChangeText={handleInputChange}
+                                    value={formData.age.toString()}
+                                    keyboardType='numeric'
+                                    onChangeText={(age) => setFormData({ ...formData, age })}
                                 />
+                                {emailError ? <Text className="text-red-500 pb-2 pl-4">{emailError}</Text> : null}
                                 <TextInput
                                     placeholder="email"
                                     className="mb-3 border border-gray-300 rounded-full px-6 py-2 text-md text-gray-500"
-                                // value={inputText}
-                                // onChangeText={handleInputChange}
+                                    value={formData.email}
+                                    onChangeText={(email) => setFormData({ ...formData, email })}
                                 />
-                                <TextInput
-                                    placeholder="password"
-                                    className="border border-gray-300 rounded-full px-6 py-2 text-md text-gray-500"
-                                    secureTextEntry
-                                // value={inputText}
-                                // onChangeText={handleInputChange}
-                                />
+                                {passwordError ? <Text className="text-red-500 pl-4 ">{passwordError}</Text> : null}
+
+                                <View className=" border border-gray-300 rounded-full text-md text-gray-500 font-semibold flex flex-row items-center">
+                                    <TextInput
+                                        placeholder="password"
+                                        className="rounded-full px-6 py-2 text-md text-gray-500 font-semibold w-[90%]"
+                                        secureTextEntry={!showPassword}
+                                        value={formData.password}
+                                        onChangeText={(password) => setFormData({ ...formData, password })}
+                                        />
+                                    <TouchableOpacity onPress={handleShowPassword}>
+                                        <Icon name={showPassword ? "eye" : "eye-slash"} size={24} color="#333333B2" />
+                                    </TouchableOpacity>
+                                </View>
                                 <TextInput
                                     placeholder="confirm password"
                                     className="mt-3 border border-gray-300 rounded-full px-6 py-2 text-md text-gray-500"
                                     secureTextEntry
-                                // value={inputText}
-                                // onChangeText={handleInputChange}
+                                    value={confirmPassword}
+                                    onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)}
                                 />
-                                <TouchableOpacity className="shadow shadow-gray-800 mt-7 mb-5 w-10px h-auto py-2 shadow-3xl   bg-gray-800 rounded-full ">
-                                    <Text className="text-lg text-center text-gray-50 font-md">
-                                        sign up {" "}
-                                        <Icon name="arrow-right" size={17} color="#fff" className="font-light" />
+                                <TouchableOpacity onPress={handleSignUp} className="shadow shadow-gray-800 mt-7 mb-5 w-10px h-auto py-2 shadow-3xl   bg-gray-800 rounded-full ">
+                                    {isLoading ? (
+                                        <ActivityIndicator color="#ffffff" />
+                                    ) : (
+                                        <Text className="text-lg text-center text-gray-50 font-md">
+                                            sign up {" "}
+                                            <Icon name="arrow-right" size={17} color="#fff" className="font-light" />
 
-                                    </Text>
+                                        </Text>
+                                    )}
                                 </TouchableOpacity>
                                 <View className="flex flex-col justify-between items-center gap-3 mb-5">
                                     <Text className="text-md text-gray-600 ">Already have an account? <Link href={'/screens/login'} className="underline underline-offset-4 text-blue-600 ">login</Link></Text>
