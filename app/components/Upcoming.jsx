9dfe1dcab255a929bfae2b96668ba708d2a25566
Antony Gakuru <antony123muriithi@gmail.com>
2024-02-28 16:42:59 +0300

    fix home cards

--- app/components/Upcoming.jsx
+++ app/components/Upcoming.jsx
@@ -9,29 +9,32 @@ const Upcoming = () => {
   return (
     <View className="w-full h-40 py-3">
       <ScrollView className="flex-1 gap-4 " horizontal >
-        <View style={styles.shadow} className="justify-center items-center px-2 h-32 bg-blue-200 dark:bg-blue-950 rounded-lg" key="1">
-          <View className="rounded-full bg-gray-100 p-2">
-            <Text className="text-3xl font-semibold text-gray-700">12</Text>
+        <View style={styles.shadow} className="justify-center items-center px-2 w-40 h-32 bg-blue-200 dark:bg-blue-950 rounded-lg" key="1">
+          <View className="rounded-full bg-gray-100 dark:bg-gray-950 p-2">
+            <Text className="w-7 h-7 text-center text-2xl font-semibold text-gray-600 dark:text-gray-300">12</Text>
           </View>
-          <Text className="text-lg mx-2 dark:text-gray-300 ">Assignments</Text>
+          <Text className="text-lg text-gray-600 py-2 mx-2 dark:text-gray-300 ">Assignments</Text>
         </View>
-        <View style={styles.shadow} className="justify-center items-center px-2 h-32 bg-red-100 dark:bg-red-950 rounded-lg my-2" key="2">
-          <View className="rounded-full bg-gray-100 p-2">
-            <Text className="text-3xl font-semibold text-gray-700">18</Text>
+
+        <View style={styles.shadow} className="justify-center items-center px-2 w-40 h-32 bg-blue-200 dark:bg-blue-950 rounded-lg" key="2">
+          <View className="rounded-full bg-gray-100 dark:bg-gray-950 p-2">
+            <Text className="w-7 h-7 text-center text-2xl font-semibold text-gray-600 dark:text-gray-300">18</Text>
           </View>
-          <Text className="text-lg mx-2 dark:text-gray-300">Tasks</Text>
+          <Text className="text-lg text-gray-600 py-2 mx-2 dark:text-gray-300 ">Tasks</Text>
         </View>
-        <View style={styles.shadow} className="justify-center items-center px-2 h-32 bg-green-200 dark:bg-green-950 rounded-lg" key="3">
-          <View className="rounded-full bg-gray-100 p-2">
-            <Text className="text-3xl font-semibold text-gray-700">7</Text>
+
+        <View style={styles.shadow} className="justify-center items-center px-2 w-40 h-32 bg-blue-200 dark:bg-blue-950 rounded-lg" key="3">
+          <View className="rounded-full bg-gray-100 dark:bg-gray-950 p-2">
+            <Text className="w-7 h-7 text-center text-2xl font-semibold text-gray-600 dark:text-gray-300">7</Text>
           </View>
-          <Text className="text-lg mx-2 dark:text-gray-300">Bills</Text>
+          <Text className="text-lg text-gray-600 py-2 mx-2 dark:text-gray-300 ">Bills</Text>
         </View>
-        <View style={styles.shadow} className="justify-center items-center px-2 h-32 bg-yellow-100 dark:bg-yellow-950 shadow rounded-lg" key="4">
-          <View className="rounded-full bg-gray-100 p-2">
-            <Text className="text-3xl font-semibold text-gray-700">1</Text>
+        
+        <View style={styles.shadow} className="justify-center items-center px-2 w-40 h-32 bg-blue-200 dark:bg-blue-950 rounded-lg" key="4">
+          <View className="rounded-full bg-gray-100 dark:bg-gray-950 p-2">
+            <Text className="w-7 h-7 text-center text-2xl font-semibold text-gray-600 dark:text-gray-300">1</Text>
           </View>
-          <Text className="text-lg mx-2 dark:text-gray-300">Examinations</Text>
+          <Text className="text-lg text-gray-600 py-2 mx-2 dark:text-gray-300 ">Examinations</Text>
         </View>
       </ScrollView>
     </View>
