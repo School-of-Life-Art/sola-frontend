219566020552fedfaae376e47ee26cf3dae96751
Antony Gakuru <antony123muriithi@gmail.com>
2024-03-11 18:24:17 +0300

    add goals

--- app/components/goals.jsx
+++ app/components/goals.jsx
@@ -2,7 +2,6 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React, { memo, useEffect, useState } from 'react'
 import { ScrollView } from 'react-native-gesture-handler'
 import { Agenda, DateData, AgendaEntry, AgendaSchedule } from 'react-native-calendars';
-import { Card, Avatar } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { connect } from 'react-redux';
 import BASE_URL from '../baseUrl';
@@ -19,6 +18,18 @@ const Goals = ({ user, theme }) => {
     const date = new Date(time);
     return date.toISOString().split('T')[0];
   };
+
+  const getDateRange = () => {
+    let today = new Date();
+    const before = new Date(today);
+    before.setMonth(before.getMonth() - 4);
+
+    const after = new Date(today);
+    after.setMonth(after.getMonth() + 4);
+
+    return [before, after];
+  }
+  const [start_date, end_date] = getDateRange();
   // const items = {
   //   '2024-03-06': [{ name: 'Email Lookout CEO' }, { name: 'Hand over keys to Janitor Omondi' }, { name: 'Submit application for Chief of Staff' },],
   //   '2024-03-08': [{ name: 'Check into AirBnB', height: 80 }],
@@ -35,15 +46,25 @@ const Goals = ({ user, theme }) => {
     // Fetch goals from Rails backend
     const fetchGoals = async () => {
       try {
-        const response = await fetch(`${BASE_URL}/api/v1/tasks`);
+        const response = await fetch(`${BASE_URL}/api/v1/goals/agenda`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Bearer ${user.jwt}`
+          },
+          body: JSON.stringify({
+            start_date, end_date
+          })
+        });
         if (response.ok) {
           const goals = await response.json();
           // Organize goals into the desired format
           const formattedGoals = {};
           goals.forEach(goal => {
-            const startDate = goal.start_date.split('T')[0]; 
+            const startDate = goal.start_date.split('T')[0];
             if (!formattedGoals[startDate]) {
-              formattedGoals[startDate] = [{ name: goal.title, start_time: formatTime(goal.start_date), end_date: formatTime(goal.end_date)} ];
+              formattedGoals[startDate] = [{ name: goal.title, start_time: formatTime(goal.start_date), end_date: formatTime(goal.end_date) }];
             } else {
               formattedGoals[startDate].push({ name: goal.title, start_time: formatTime(goal.start_date), end_date: formatTime(goal.end_date) });
             }
@@ -67,76 +88,71 @@ const Goals = ({ user, theme }) => {
 
   const loadItems = (day) => {
   };
-  const RenderItem = ({item}) => {
-      return (
-        <View
-          style={{
-            elevation: 3,
-            shadowOffset: { width: 0, height: 2 },
-            shadowOpacity: 0.3,
-            shadowRadius: 4,
-            marginRight: 10,
-            marginTop: 17,
-          }} className={`bg-slate-100 dark:bg-slate-900 flex flex-row justify-between items-center rounded-xl py-4   px-2 border-l-4 border-green-300`}>
-          <View className="px-4">
-            <Text className="text-xs  uppercase text-slate-700 dark:text-slate-100">Goal</Text>
-            <Text className="text-lg text-slate-00 dark:text-slate-100 ">{item.name}</Text>
-            <Text className="text-xs font-light text-slate-700 dark:text-slate-100 ">{item.start_time}-{item.end_date}</Text>
-          </View>
-          <TouchableOpacity className="pr-5">
-            <Icon name="bell" size={24} color="coral" />
-          </TouchableOpacity>
+  const RenderItem = ({ item }) => {
+    return (
+      <View
+        style={{
+          elevation: 3,
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.3,
+          shadowRadius: 4,
+          marginRight: 10,
+          marginTop: 17,
+        }} className={`bg-slate-100 dark:bg-slate-900 flex flex-row justify-between items-center rounded-xl py-4   px-2 border-l-4 border-green-300`}>
+        <View className="px-4">
+          <Text className="text-xs  uppercase text-slate-700 dark:text-slate-100">Goal</Text>
+          <Text className="text-lg text-slate-00 dark:text-slate-100 ">{item.name}</Text>
+          <Text className="text-xs font-light text-slate-700 dark:text-slate-100 ">{item.start_time}-{item.end_date}</Text>
         </View>
-      );
+        <TouchableOpacity className="pr-5">
+          <Icon name="bell" size={24} color="coral" />
+        </TouchableOpacity>
+      </View>
+    );
   };
 
   const MemoizedRenderItem = memo(RenderItem)
   const renderItem = (item) => {
     return <MemoizedRenderItem item={item} />;
   };
-  
+
 
   const renderEmptyData = () => {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text className="text-gray-700 dark:text-gray-400">Nothing planned for this date</Text>
-        </View>
-      );
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text className="text-gray-700 dark:text-gray-400">Nothing planned for this date</Text>
+      </View>
+    );
   };
 
-  const today = new Date();
-  const minDate = new Date(today.getFullYear(), today.getMonth() - 3, 1);
-  const maxDate = new Date(today.getFullYear(), today.getMonth() + 6, 0);
-  
-  
   return (
 
-      <Agenda
-        items={items}
-        loadItemsForMonth={loadItems}
-        selected={`${new Date()}`}
-        showClosingKnob={true}
-        hideKnob={false}
-        renderItem={renderItem}
-        minDate={minDate.toDateString()}
-        maxDate={maxDate.toDateString()}
-        style={{ backgroundColor: 'transparent' }}
-        theme={{
-          backgroundColor: theme === 'light' ? '#f1f5f9' : '#0f172a',
-          calendarBackground: theme === 'light' ? '#f1f5f9' : '#0f172a',
-          textSectionTitleColor: '#b6c1cd',
-          selectedDayBackgroundColor: '#00adf5',
-          selectedDayTextColor: '#ffffff',
-          // selectedDayTextColor: theme === 'light' ? '#f1f5f9' : '#0f172a',
-          todayTextColor: '#00adf5',
-          dayTextColor: theme === 'light' ? '#2d4150': '#ffffffb3',
-          reservationsBackgroundColor: theme === 'light' ? '#f1f5f9' : '#0f172a'
+    <Agenda
+      items={items}
+      loadItemsForMonth={loadItems}
+      selected={`${new Date()}`}
+      showClosingKnob={true}
+      hideKnob={false}
+      renderItem={renderItem}
+      minDate={start_date.toDateString()}
+      maxDate={end_date.toDateString()}
+      style={{ backgroundColor: 'transparent' }}
+      theme={{
+        backgroundColor: theme === 'light' ? '#f1f5f9' : '#0f172a',
+        calendarBackground: theme === 'light' ? '#f1f5f9' : '#0f172a',
+        textSectionTitleColor: '#b6c1cd',
+        selectedDayBackgroundColor: '#00adf5',
+        selectedDayTextColor: '#ffffff',
+        // selectedDayTextColor: theme === 'light' ? '#f1f5f9' : '#0f172a',
+        todayTextColor: '#00adf5',
+        dayTextColor: theme === 'light' ? '#2d4150' : '#ffffffb3',
+        reservationsBackgroundColor: theme === 'light' ? '#f1f5f9' : '#0f172a'
 
-          // textDisabledColor: '#d9e'
-        }}
-        renderEmptyData={renderEmptyData}
+        // textDisabledColor: '#d9e'
+      }}
+      renderEmptyData={renderEmptyData}
 
-      />
+    />
 
 
   )
